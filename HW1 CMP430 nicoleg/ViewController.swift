127a39a49289c0921ac39e6eec54a9fb7018f93c
Nicole G <gonzalez.s.nicole@gmail.com>
2019-09-23 17:52:26 -0400

    Initial Commit

---
+++ HW1 CMP430 nicoleg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW1 CMP430 nicoleg
+//
+//  Created by Nicole Gonzalez on 9/23/19.
+//  Copyright © 2019 Nicole Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
